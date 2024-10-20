4dcdb5ee0c8940af62fd99be28a151c9c010c6aa
João Igor <joaoigor21052004@gmail.com>
2024-10-20 19:56:05 -0300

    commit2

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get("/pagina3", (req, res) => {
     res.send('<h1>Terceira Pagina!</h1>');
 });
 
+app.get("/pagina4", (req, res) => {
+    res.send('<h1>Americo!</h1>');
+});
+
 app.listen(3000, () => {
     console.log("Servidor rodando na http://localhost:3000");
 });
