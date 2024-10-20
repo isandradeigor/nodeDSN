8c590947874cdc28c3b421eb761c923550081a27
João Igor Andrade <110249417+isandradeigor@users.noreply.github.com>
2024-10-20 19:14:05 -0300

    node

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send('<h1>Navegue entre as paginas!</h1>');
+});
+
+app.get("/pagina1", (req, res) => {
+    res.send('<h1>Primeira Pagina!</h1>');
+});
+
+app.get("/pagina2", (req, res) => {
+    res.send('<h1>Segunda Pagina!</h1>');
+});
+
+app.get("/pagina3", (req, res) => {
+    res.send('<h1>Terceira Pagina!</h1>');
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na http://localhost:3000");
+});
